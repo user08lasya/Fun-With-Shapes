9f0ce661a9a35c0eaf9ff747733447d2c6a58e53
user08lasya <lasyapriya0807@gmail.com>
2024-10-22 21:53:47 +0530

    shapes

---
+++ shapes.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.Screen().bgcolor("Orange")
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.title("Polygons")
+board = turtle.Turtle()
+for i in range(3):
+    board.forward(100)
+    board.left(120)
+    i = i+1
+    
+for i in range(5):
+    board.forward(100)
+    board.left(72)
+    i = i+1
+
+   
